563e61233d84572f81d9aebecb52f65dd212e337
Soumya Dayal <sd9829@rit.edu>
2022-01-29 13:21:16 -0500

    created StorageManager.java

---
+++ storagemanager/StorageManager.java
@@ -0,0 +1,52 @@
+package storagemanager;
+
+import common.ITable;
+
+import java.util.ArrayList;
+
+public class StorageManager extends AStorageManager{
+    @Override
+    public boolean clearTableData(ITable table) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Object> getRecord(ITable table, Object pkValue) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<ArrayList<Object>> getRecords(ITable table) {
+        return null;
+    }
+
+    @Override
+    public boolean insertRecord(ITable table, ArrayList<Object> record) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteRecord(ITable table, Object primaryKey) {
+        return false;
+    }
+
+    @Override
+    public boolean updateRecord(ITable table, ArrayList<Object> oldRecord, ArrayList<Object> newRecord) {
+        return false;
+    }
+
+    @Override
+    public void purgePageBuffer() {
+
+    }
+
+    @Override
+    public boolean addAttributeValue(ITable table, Object defaultValue) {
+        return false;
+    }
+
+    @Override
+    public boolean dropAttributeValue(ITable table, int attrIndex) {
+        return false;
+    }
+}
