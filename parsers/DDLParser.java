c5f1c6df3b0d3470badecd775494ffa45f7a55ee
Soumya Dayal <sd9829@rit.edu>
2022-02-19 14:02:50 -0500

    rudimentary drop table in DDL parser.

--- parsers/DDLParser.java
+++ parsers/DDLParser.java
@@ -1,5 +1,10 @@
 package parsers;
 
+import catalog.ACatalog;
+import catalog.Catalog;
+import common.Table;
+import storagemanager.AStorageManager;
+
 /*
   Class for DDL parser
 
@@ -13,12 +18,28 @@ package parsers;
  */
 public class DDLParser {
 
+    private static ACatalog catalog;
+    private static AStorageManager sm;
+
+    public static boolean dropInstruction(String tableName){
+        catalog.dropTable(tableName);
+        return true;
+    }
+
     /**
      * This function will parse and execute DDL statements (create table, create index, etc)
      * @param stmt the statement to parse
      * @return true if successfully parsed/executed; false otherwise
      */
     public static boolean parseDDLStatement(String stmt){
+        String instruction = stmt.split(" ")[0];
+        if(instruction.equals("create")){
+            //create <smth>
+        }
+        else if (instruction.equals("drop")){
+            dropInstruction(stmt.split(" ")[2]);
+        }
+
         return true;
     }
 }
