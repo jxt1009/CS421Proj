2542832898b766a6f3c0324c2c06d2fc3061aeba
Soumya Dayal <sd9829@rit.edu>
2022-03-18 12:50:22 -0400

    made funciton stub

--- parsers/DMLParser.java
+++ parsers/DMLParser.java
@@ -289,9 +289,15 @@ public class DMLParser {
      * @return the data resulting from the query; null upon error.
      * Note: No data and error are two different cases.
      */
-    //NOT FOR THIS PHASE - EVERYTHING IN THE SECOND PHASE HAS TO BE IMPLEMENTED
-    //IN THE PARSEDMLSTATEMENT FUNCTION
     public static ResultSet parseDMLQuery(String query) {
+        return null;
+    }
+
+    public static ResultSet parseSelectClause(String query){
+        return null;
+    }
+
+    public static ResultSet parseFromClause(String query){
         return null;
     }
 }
